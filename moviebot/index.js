1e185b7dc3e4c3f4ead406368118e48886fcbb99
Mathis Neumann <mathis@simpletechs.net>
2016-02-27 18:48:13 +0100

    basic people request

--- moviebot/index.js
+++ moviebot/index.js
@@ -1,9 +1,12 @@
 'use strict';
 const Botkit = require('botkit');
-const request = require('request'); // for external API/HTTP requests
+const request = require('request-promise'); // for external API/HTTP requests
+const querystring = require('querystring');
 
-if (!process.env.token) {
-  console.log('Error: Specify token in environment');
+const movieDBURL = 'https://api.themoviedb.org/3/';
+
+if (!process.env.token || !process.env.moviedb_key) {
+  console.log('Error: Specify token in environment and moviedb_key');
   process.exit(1);
 }
 
@@ -20,38 +23,61 @@ controller.spawn({
 });
 
 
-controller.hears(['.*'],['direct_message','direct_mention','mention'],function(bot,message) {
-  const attachments = [];
-  const attachment = {
-    title: 'This is an attachment',
-    color: '#FFCC99',
-    fields: [],
-  };
+controller.hears(['from (.+)'],['direct_message','direct_mention','mention'],function(bot,message) {
+    console.log('got request', message);
+    bot.reply(message, 'trying my best');
 
-  attachment.fields.push({
-    label: 'Field',
-    value: 'A longish value',
-    short: false,
-  });
+    const query = {
+        'sort_by': 'popularity.desc',
+        query: message.match[1],
+        'api_key': process.env.moviedb_key
+    }
+    const requestOptions = {
+        uri: `${movieDBURL}search/person?${querystring.stringify(query)}`
+    }
+    console.log('starting request', requestOptions, query);
 
-  attachment.fields.push({
-    label: 'Field',
-    value: 'Value',
-    short: true,
-  });
+    request(requestOptions)
+        .then((results) => {
+            const people = results.results
+            console.log('found people', people);
 
-  attachment.fields.push({
-    label: 'Field',
-    value: 'Value',
-    short: true,
-  });
+        })
+        .catch((err) => {
+            console.error('oh no', err, err.stack);
+        });
 
-  attachments.push(attachment);
+    // const attachments = [];
+    // const attachment = {
+    //   title: 'This is an attachment',
+    //   color: '#FFCC99',
+    //   fields: [],
+    // };
 
-  bot.reply(message,{
-    text: 'See below...',
-    attachments: attachments,
-  },function(err,resp) {
-    console.log(err,resp);
-  });
+    // attachment.fields.push({
+    //   label: 'Field',
+    //   value: 'A longish value',
+    //   short: false,
+    // });
+
+    // attachment.fields.push({
+    //   label: 'Field',
+    //   value: 'Value',
+    //   short: true,
+    // });
+
+    // attachment.fields.push({
+    //   label: 'Field',
+    //   value: 'Value',
+    //   short: true,
+    // });
+
+    // attachments.push(attachment);
+
+    // bot.reply(message,{
+    // text: 'See below...',
+    // attachments: attachments,
+    // },function(err,resp) {
+    // console.log(err,resp);
+    // });
 });
