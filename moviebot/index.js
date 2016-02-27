f628379b56c6feed6fefb9ca8f0aad02561af5d7
Mathis Neumann <mathis@simpletechs.net>
2016-02-27 17:19:04 +0100

    moviebot beginning

---
+++ moviebot/index.js
@@ -0,0 +1,57 @@
+'use strict';
+const Botkit = require('botkit');
+const request = require('request'); // for external API/HTTP requests
+
+if (!process.env.token) {
+  console.log('Error: Specify token in environment');
+  process.exit(1);
+}
+
+const controller = Botkit.slackbot({
+ debug: false
+});
+
+controller.spawn({
+  token: process.env.token
+}).startRTM(function(err) {
+  if (err) {
+    throw new Error(err);
+  }
+});
+
+
+controller.hears(['.*'],['direct_message','direct_mention','mention'],function(bot,message) {
+  const attachments = [];
+  const attachment = {
+    title: 'This is an attachment',
+    color: '#FFCC99',
+    fields: [],
+  };
+
+  attachment.fields.push({
+    label: 'Field',
+    value: 'A longish value',
+    short: false,
+  });
+
+  attachment.fields.push({
+    label: 'Field',
+    value: 'Value',
+    short: true,
+  });
+
+  attachment.fields.push({
+    label: 'Field',
+    value: 'Value',
+    short: true,
+  });
+
+  attachments.push(attachment);
+
+  bot.reply(message,{
+    text: 'See below...',
+    attachments: attachments,
+  },function(err,resp) {
+    console.log(err,resp);
+  });
+});
